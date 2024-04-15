8fbbc9bf8393f4e7f2a4fff45c694e1692d7f770
David <davidvillegasr@gmail.com>
2024-04-15 16:51:40 -0400

    Directories created for the different classes. insert query done in connected mode

---
+++ TP_DB_CONNECTIONAPP/TP_DB_CONNECTION/modele/Passager.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP_DB_CONNECTION.modele
+{
+    internal class Passager
+    {
+        public string Nom { get; set; }
+        public string Prenom { get; set; }
+        public string Adresse { get; set; }
+        public string Ville { get; set; }
+        public string Pays { get; set; }
+        public string Statut { get; set; }
+
+        public Passager()
+        {
+            
+        }
+
+        public Passager(string nom, string prenom, string adresse, string ville, string pays, string statut)
+        {
+            Nom = nom;
+            Prenom = prenom;
+            Adresse = adresse;
+            Ville = ville;
+            Pays = pays;
+            Statut = statut;
+        }
+
+    }
+}
---
+++ TP_DB_CONNECTIONAPP/TP_DB_CONNECTION/ui/InsertPassager.cs
@@ -0,0 +1,55 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TP_DB_CONNECTION
+{
+    public partial class InsertPassager : Form
+    {
+        
+        public InsertPassager()
+        {
+            InitializeComponent();
+
+        }
+        Dao Dao = new Dao("server=localhost;userid=root;database=gestionreservation;port=3306");
+
+        private void btn_retour_Click(object sender, EventArgs e)
+        {
+            this.Visible = false;
+        }
+
+        private void btn_inserer_Click(object sender, EventArgs e)
+        {
+            Dao.GetConnection();
+            string v_nom = txt_nom.Text;
+            string v_prenom = txt_prenom.Text;
+            string v_adresse = txt_adresse.Text;
+            string v_ville = txt_ville.Text;
+            string v_pays = txt_pays.Text;
+            string v_statut = cmb_statut.Text;
+
+            string execute = "INSERT INTO passager (nom, prenom, adresse, ville, pays, statut) VALUES ('" + v_nom + "', '" + v_prenom + "', '" + v_adresse + "', '" + v_ville + "', '" + v_pays + "', '" + v_statut + "')";
+            MySqlCommand cmd = new MySqlCommand(execute, Dao.conn);
+            int insertedrows = cmd.ExecuteNonQuery();
+            if (insertedrows > 0)
+            {
+                MessageBox.Show("Insertion reussie");
+            }
+            else
+            {
+                MessageBox.Show("Insertion echouee");
+            }
+
+
+            Dao.EndConnection();
+        }
+    }
+}
