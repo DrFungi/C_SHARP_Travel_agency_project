9e2c0fab0dade12ff3ae464bf7015ca94da3de15
Hajar Rebouze <hajar.idbourkha@gmail.com>
2024-04-15 21:15:39 -0400

    updating insertPassager

--- TP_DB_CONNECTIONAPP/TP_DB_CONNECTION/ui/InsertPassager.cs
+++ TP_DB_CONNECTIONAPP/TP_DB_CONNECTION/ui/InsertPassager.cs
@@ -13,7 +13,7 @@ namespace TP_DB_CONNECTION
 {
     public partial class InsertPassager : Form
     {
-        
+
         public InsertPassager()
         {
             InitializeComponent();
@@ -28,28 +28,82 @@ namespace TP_DB_CONNECTION
 
         private void btn_inserer_Click(object sender, EventArgs e)
         {
-            Dao.GetConnection();
-            string v_nom = txt_nom.Text;
-            string v_prenom = txt_prenom.Text;
-            string v_adresse = txt_adresse.Text;
-            string v_ville = txt_ville.Text;
-            string v_pays = txt_pays.Text;
-            string v_statut = cmb_statut.Text;
-
-            string execute = "INSERT INTO passager (nom, prenom, adresse, ville, pays, statut) VALUES ('" + v_nom + "', '" + v_prenom + "', '" + v_adresse + "', '" + v_ville + "', '" + v_pays + "', '" + v_statut + "')";
-            MySqlCommand cmd = new MySqlCommand(execute, Dao.conn);
-            int insertedrows = cmd.ExecuteNonQuery();
-            if (insertedrows > 0)
+            try
             {
-                MessageBox.Show("Insertion reussie");
+                Dao.GetConnection();
+
+                string execute = "INSERT INTO passager (nom, prenom, adresse, ville, pays, statut)" +
+                    " VALUES (@v_nom,@v_prenom,@v_adresse,@v_ville,@v_pays,@v_statut)";
+                string v_nom = txt_nom.Text;
+                string v_prenom = txt_prenom.Text;
+                string v_adresse = txt_adresse.Text;
+                string v_ville = txt_ville.Text;
+                string v_pays = txt_pays.Text;
+                string v_statut = cmb_statut.Text;
+
+                MySqlParameter p1 = new MySqlParameter();
+                p1.ParameterName = "@v_nom";
+                p1.Value = v_nom;
+
+                MySqlParameter p2 = new MySqlParameter();
+                p2.ParameterName = "@v_prenom";
+                p2.Value = v_prenom;
+
+                MySqlParameter p3 = new MySqlParameter();
+                p3.ParameterName = "@v_adresse";
+                p3.Value = v_adresse;
+
+                MySqlParameter p4 = new MySqlParameter();
+                p4.ParameterName = "@v_ville";
+                p4.Value = v_ville;
+
+                MySqlParameter p5 = new MySqlParameter();
+                p5.ParameterName = "@v_pays";
+                p5.Value = v_pays;
+
+                MySqlParameter p6 = new MySqlParameter();
+                p6.ParameterName = "@v_statut";
+                p6.Value = v_statut;
+
+                MySqlCommand cmd = new MySqlCommand(execute, Dao.conn);
+
+                cmd.Parameters.Add(p1);
+                cmd.Parameters.Add(p2);
+                cmd.Parameters.Add(p3);
+                cmd.Parameters.Add(p4);
+                cmd.Parameters.Add(p5);
+                cmd.Parameters.Add(p6);
+
+                int insertedrows = cmd.ExecuteNonQuery();
+                Console.WriteLine("{0} ligne ajoutée", insertedrows);
+
+                if (insertedrows > 0)
+                {
+                    MessageBox.Show("Insertion reussie");
+                }
+                else
+                {
+                    MessageBox.Show("Insertion echouee");
+                }
+
+
             }
-            else
+            catch (Exception ex)
             {
-                MessageBox.Show("Insertion echouee");
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                Dao.EndConnection();
             }
 
 
-            Dao.EndConnection();
+
+        }
+
+        private void cmb_statut_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
